80ac5c0fc1b5d4392ec7c138db952b3fc9ef5a2f
王涛 <rushui@qq.com>
2023-04-11 22:50:29 +0800

    update tsconfig.json

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,15 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import App from './App.vue'
 import router from './router'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import './assets/main.css'
 
 const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
