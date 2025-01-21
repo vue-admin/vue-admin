b584f1ed3926442fc43d0354bbcb715faf2a8cf3
王涛 <rushui@qq.com>
2025-01-21 16:23:20 +0800

    crud

--- src/layout/Index.vue
+++ src/layout/Index.vue
@@ -1,17 +1,19 @@
 <template>
-  <el-container>
-    <Sidebar />
+  <el-watermark :font="font" :content="['wang', 'vue-admin']">
     <el-container>
-      <el-header>
-        <Header />
-      </el-header>
-      <TagView />
-      <el-main>
-        <RouterView />
-      </el-main>
-      <Footer />
+      <Sidebar />
+      <el-container>
+        <el-header>
+          <Header />
+        </el-header>
+        <TagView />
+        <el-main>
+          <RouterView />
+        </el-main>
+        <Footer />
+      </el-container>
     </el-container>
-  </el-container>
+  </el-watermark>
 </template>
 
 <script setup lang="ts">
@@ -19,4 +21,22 @@ import Sidebar from './components/Sidebar/Index.vue'
 import Header from './components/Header/Index.vue'
 import TagView from './components/TagsView/Index.vue'
 import Footer from './components/Footer.vue'
+import { reactive, watch } from 'vue'
+import { isDark } from '@/stores/dark'
+
+const font = reactive({
+  color: 'rgba(0, 0, 0, .05)'
+})
+
+watch(
+  isDark,
+  () => {
+    font.color = isDark.value
+      ? 'rgba(255, 255, 255, .05)'
+      : 'rgba(0, 0, 0, .05)'
+  },
+  {
+    immediate: true
+  }
+)
 </script>
