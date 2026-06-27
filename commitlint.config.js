09f12e5c6d87b7046ce57b7fe0252b23eab5106f
如水 <rushui@qq.com>
2026-06-27 21:04:13 +0800

    chore(commit): add commitlint and husky commit-msg hook

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
+    'subject-case': [0],
+    'header-max-length': [2, 'always', 100],
+    'body-max-line-length': [0],
+  },
+}
